ebbc217e6851599c687aed8b21645283e94c03b3
Fekete Áron <aron.fekete@stud.uni-corvinus.hu>
2021-03-18 13:54:03 +0100

    jsgyak_i7s4ed

---
+++ HajosTeszt/HajosTeszt/wwwroot/jsgyak.js
@@ -0,0 +1,30 @@
+﻿var faktoriálisR = (n) => {
+    if (n === 0 || n === 1) {
+        return 1;
+    } else {
+        return n * faktoriálisR(n - 1)
+    }
+}
+
+
+window.onload = function () {
+    let element = document.getElementById("pascal")
+
+    for (var s = 0; s < 10; s++) {
+        let sor = document.createElement("div");
+        sor.classList.add("sor")
+        element.appendChild(sor)
+        console.log(s);
+
+
+        for (var oszlop = 0; oszlop <= s; oszlop++) {
+            let elem = document.createElement("div")
+            elem.classList.add("elem")
+            elem.innerText = faktoriálisR(s) /(faktoriálisR(oszlop) * faktoriálisR(s-oszlop))
+            sor.appendChild(elem);
+            elem.style.color=`rgb(${255/10*s},0,${255/10*oszlop})`
+        }
+    }
+
+   
+}
