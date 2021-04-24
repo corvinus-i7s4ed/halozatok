080c98654922002a39d519bc7199faccb0f7af2c
Fekete Áron <aron.fekete@stud.uni-corvinus.hu>
2021-04-24 12:28:01 +0200

    kilencedik I7S4ED

--- HajosTeszt/HajosTeszt/Controllers/BoatController.cs
+++ HajosTeszt/HajosTeszt/Controllers/BoatController.cs
@@ -20,5 +20,21 @@ namespace HajosTeszt.Controllers
             var kérdések = from x in context.Questions select x.QuestionText;
             return new JsonResult(kérdések);
         }
+
+        [HttpGet]
+        [Route("questions/{sorszám}")]
+        public ActionResult M2(int sorszám)
+        {
+            HajostesztContext context = new HajostesztContext();
+            var kérdés = (from x in context.Questions
+                          where x.QuestionId == sorszám
+                          select x).FirstOrDefault();
+            if (kérdés == null) return BadRequest("Nincs ilyen sorszámú kérdés");
+
+            return new JsonResult(kérdés);
+            
+
+            
+        }
     }
 }
